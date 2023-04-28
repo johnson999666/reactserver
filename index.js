41b68d3d1d8122af5b0c579e6adbcc2f841ee321
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-04-28 18:30:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+
+
+app.get('/', (req, res) => {
+  res.render('index', { name: 'World' });
+});
+
+app.get('/first', (req, res) => {
+  res.render('first', { name: 'World' });
+});
+
+app.get('/me', (req, res) => {
+  res.render('me', { name: 'World' });
+});
+
+app.get('/second', (req, res) => {
+  res.render('second', { name: 'World' });
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});

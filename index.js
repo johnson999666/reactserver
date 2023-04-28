e853acd02cf50c1ff81224fd8d221850ab7527a2
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-04-28 16:16:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.get('/second', (req, res) => {
 
 
 
-app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+app.listen(8000, "0.0.0.0", () => {
   console.log('Server listening on port 3000');
 });
